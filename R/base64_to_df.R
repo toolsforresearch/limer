d92be6f52ad50e1dcffdca9d7af4fbe3a93931d7
Jan-E <github@ehrhardt.nl>
2018-07-27 01:24:09 +0200

    test if copy_survey exists to detect LS3

--- R/base64_to_df.R
+++ R/base64_to_df.R
@@ -11,5 +11,12 @@
 base64_to_df <- function(x) {
   raw_csv <- rawToChar(base64enc::base64decode(x))
 
-  return(read.csv(textConnection(raw_csv), stringsAsFactors = FALSE, sep = ";"))
+  have_copy_survey <- call_limer(method="copy_survey")
+  # returns NULL in LS2
+  # returns array(status => "Copy failed", error => "No survey ID has been provided. Cannot copy survey") in LS3
+  if (is.null(have_copy_survey)) {
+    return(read.csv(textConnection(raw_csv), stringsAsFactors = FALSE))
+  } else {
+    return(read.csv(textConnection(raw_csv), stringsAsFactors = FALSE, sep = ";"))
+  }
 }
