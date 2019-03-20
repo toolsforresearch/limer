7537bccb86bca4e49c8727c6be9ad7e9cb1f3a9b
Jan-E <github@ehrhardt.nl>
2019-03-20 13:35:27 +0100

    copy_survey of sid -1 returns array in LS3

--- R/base64_to_df.R
+++ R/base64_to_df.R
@@ -11,9 +11,9 @@
 base64_to_df <- function(x) {
   raw_csv <- rawToChar(base64enc::base64decode(x))
 
-  have_copy_survey <- call_limer(method="copy_survey")
+  have_copy_survey <- call_limer(method="copy_survey", params = list (iSurveyID_org = -1, sNewname = "This should fail"))
   # returns NULL in LS2
-  # returns array(status => "Copy failed", error => "No survey ID has been provided. Cannot copy survey") in LS3
+  # returns array(status => "Copy failed", error => "Invalid survey ID") in LS3
   if (is.null(have_copy_survey)) {
     return(read.csv(textConnection(raw_csv), stringsAsFactors = FALSE))
   } else {
